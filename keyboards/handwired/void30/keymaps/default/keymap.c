39fd9a1c98f32e4591783285e6c41998b7484fd5
Victor Lucachi <lucachi.victor@gmail.com>
2022-05-16 19:16:36 +0300

    update VOID30

--- keyboards/handwired/void30/keymaps/default/keymap.c
+++ keyboards/handwired/void30/keymaps/default/keymap.c
@@ -1,18 +1,6 @@
-/* Copyright 2020 Victor Lucachi
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2022 Victor Lucachi (@victorlucachi)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include QMK_KEYBOARD_H
 
 // Defines names for use in layer keycodes and the keymap
@@ -74,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* FN3
  * ,---------------------------------------------------------------------.
- * |      |      |      |      |      |   -  |   =  |  [   |   ]  |   \  | 
+ * |      |      |      |      |      |   -  |   =  |  [   |   ]  |   \  |
  * |------+------+------+------+------+------+------+------+------+------|
  * |  TAB |      |      |      |      |   ,  |   .  |   /  |   ;  |   '  |
  * |------+------+------+------+------+------+------+------+------+------|
@@ -89,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* FN4
  * ,---------------------------------------------------------------------.
- * |      |      |      |      |      |   _  |   +  |  {   |   }  |   |  | 
+ * |      |      |      |      |      |   _  |   +  |  {   |   }  |   |  |
  * |------+------+------+------+------+------+------+------+------+------|
  * |  TAB |      |      |      |      |   <  |   >  |   ?  |   ;  |   "  |
  * |------+------+------+------+------+------+------+------+------+------|
@@ -104,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* ADJUST
  * ,---------------------------------------------------------------------.
- * | RESET|      |      |      |      |      |      |      |      | PSCR | 
+ * | RESET|      |      |      |      |      |      |      |      | PSCR |
  * |------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------|
@@ -121,17 +109,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _FN2, _FN1, _ADJUST);
 }
-
-/*
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-bool led_update_user(led_t led_state) {
-    return true;
-}
-*/
