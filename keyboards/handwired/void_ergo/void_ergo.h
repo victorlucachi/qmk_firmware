877e1371207dc26068a713b17da74d136bdff259
Victor <lucachi.victor@gmail.com>
2022-10-20 16:48:03 +0300

    removed unnecessary description

--- keyboards/handwired/void_ergo/void_ergo.h
+++ keyboards/handwired/void_ergo/void_ergo.h
@@ -5,15 +5,6 @@
 
 #include "quantum.h"
 
-/* This is a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
-
 #define LAYOUT_split_3x6_5( \
     L00, L01, L02, L03, L04, L05, R00, R01, R02, R03, R04, R05, \
     L10, L11, L12, L13, L14, L15, R10, R11, R12, R13, R14, R15, \
