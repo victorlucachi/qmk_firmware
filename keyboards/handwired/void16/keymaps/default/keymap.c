bb92edce13af21ba8bca42d9e30f4d8dae6e14a7
victorlucachi <lucachi.victor@gmail.com>
2022-12-02 14:37:07 +0200

    keymap cleanup for void keyboards

--- keyboards/handwired/void16/keymaps/default/keymap.c
+++ keyboards/handwired/void16/keymaps/default/keymap.c
@@ -1,6 +1,3 @@
-// Copyright 2022 Victor Lucachi (@victorlucachi)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
 
 // Defines names for use in layer keycodes and the keymap
@@ -12,78 +9,44 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ BS│
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ESC│
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │TAB│
+     * ├───┼───┼───┼───┤
+     * │FN │ 0 │ . │RET│
+     * └───┴───┴───┴───┘
+     */
+    [_BASE] = LAYOUT_ortho_4x4(
+        KC_P7,    KC_P8,  KC_P9,    KC_BSPC,
+        KC_P4,    KC_P5,  KC_P6,    KC_ESC,
+        KC_P1,    KC_P2,  KC_P3,    KC_TAB,
+        TT(_FN1), KC_P0,  KC_PDOT,  KC_ENTER
+    ),
 
-/* Base
- * ┌───┬───┬───┬───┐
- * │ 7 │ 8 │ 9 │ BS│
- * ├───┼───┼───┼───┤
- * │ 4 │ 5 │ 6 │ESC│
- * ├───┼───┼───┼───┤
- * │ 1 │ 2 │ 3 │TAB│
- * ├───┼───┼───┼───┤
- * │FN │ 0 │ . │RET│
- * └───┴───┴───┴───┘
- */
-[_BASE] = LAYOUT_ortho_4x4(
-    KC_P7,    KC_P8,  KC_P9,    KC_BSPC,
-    KC_P4,    KC_P5,  KC_P6,    KC_ESC,
-    KC_P1,    KC_P2,  KC_P3,    KC_TAB,
-    TT(_FN1), KC_P0,  KC_PDOT,  KC_ENTER
-),
+    [_FN1] = LAYOUT_ortho_4x4(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS
+    ),
 
-/* FN
- * ┌───┬───┬───┬───┐
- * │   │   │   │   │
- * ├───┼───┼───┼───┤
- * │   │   │   │   │
- * ├───┼───┼───┼───┤
- * │   │   │   │   │
- * ├───┼───┼───┼───┤
- * │   │   │   │   │
- * └───┴───┴───┴───┘
- */
-[_FN1] = LAYOUT_ortho_4x4(
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS
-),
+    [_FN2] = LAYOUT_ortho_4x4(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS
+    ),
 
-/* FN
- * ┌───┬───┬───┬───┐
- * │   │   │   │   │
- * ├───┼───┼───┼───┤
- * │   │   │   │   │
- * ├───┼───┼───┼───┤
- * │   │   │   │   │
- * ├───┼───┼───┼───┤
- * │   │   │   │   │
- * └───┴───┴───┴───┘
- */
-[_FN2] = LAYOUT_ortho_4x4(
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS
-),
-
-/* FN
- * ┌───┬───┬───┬───┐
- * │   │   │   │   │
- * ├───┼───┼───┼───┤
- * │   │   │   │   │
- * ├───┼───┼───┼───┤
- * │   │   │   │   │
- * ├───┼───┼───┼───┤
- * │   │   │   │   │
- * └───┴───┴───┴───┘
- */
-[_FN3] = LAYOUT_ortho_4x4(
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
-    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS
-),
+    [_FN3] = LAYOUT_ortho_4x4(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS
+    )
 };
 
 /*
